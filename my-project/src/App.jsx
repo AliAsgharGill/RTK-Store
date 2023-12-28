f787e157f0313f25f9734b5d64a7206a7cb7f771
aliasghargill <stylinalivlogs@gmail.com>
2023-12-28 16:17:51 +0500

    Inital Changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import Products from "./components/Products";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Products />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Products />
+      <RouterProvider router={router} />
     </>
   );
 }
--- my-project/src/components/Products.jsx
+++ my-project/src/components/Products.jsx
@@ -83,7 +83,7 @@ const Products = () => {
             </span>
             <a
               href="/"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
               Add to cart
             </a>
@@ -94,7 +94,6 @@ const Products = () => {
   ));
   return (
     <>
-      {/* {JSON.stringify(products)} */}
       <div className="p-5">
         <div className="grid grid-cols-4 gap-4 p-3">{cards}</div>
       </div>
