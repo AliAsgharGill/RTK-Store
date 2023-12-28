068d9bb1f44b56aa8a452820f8f6bc14609a20b9
aliasghargill <stylinalivlogs@gmail.com>
2023-12-28 18:16:58 +0500

    Initial Commit

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaStore } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-white border-gray-200 sticky top-0 dark:bg-gray-900">
+      <nav className="bg-white shadow-xl flex-wrap text-wrap border-gray-200 sticky top-0 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="/"
@@ -17,24 +17,20 @@ const Navbar = () => {
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <svg
-                className="w-5 h-5"
+                className="w-3.5 h-3.5 me-2"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+                fill="currentColor"
+                viewBox="0 0 18 21"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
+                <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
               </svg>
+              Buy now
             </button>
+
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
--- my-project/src/components/Products.jsx
+++ my-project/src/components/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+    import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 const Products = () => {
@@ -26,8 +26,8 @@ const Products = () => {
             </h5>
           </a>
           <div className="flex items-center mt-2.5 mb-5">
-            <div className="flex items-center space-x-1 rtl:space-x-reverse">
-              {product.rate}
+            <div className="flex items-center space-x-1 rtl:space-x-reverse text-white font-bold">
+              ({product.rating.count})
               <svg
                 className="w-4 h-4 text-yellow-300"
                 aria-hidden="true"
@@ -73,6 +73,7 @@ const Products = () => {
               >
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
               </svg>
+              {/* {product.rating.rate} */}
             </div>
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
               5.0
